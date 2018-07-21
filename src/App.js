ee8d8f66115421dcce1c804f5077de1b5cd9d796
shyam lohar <shyamlohar19996@gmail.com>
2018-07-21 17:35:52 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/navbar";
+import Counters from "./components/counters";
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <Counters />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
